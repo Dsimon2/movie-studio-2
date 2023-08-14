5dc865a1ad20c5240af2eef2e5f47d769c7caf67
Dsimon2 <browsedavid@gmail.com>
2023-08-14 19:26:08 -0400

    Nav Classname

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,3 +11,27 @@ const Header = () => {
 
   // Function to close the menu when a link is clicked
   const Close = () => setClick(false);
+
+  return (
+    <div className="nav">
+      {/* Checkbox input for the responsive menu */}
+      <input type="checkbox" id="nav-check" />
+
+      {/* Header section containing the logo */}
+      <div class="nav-header">
+        {/* Link to the home page with the logo image */}
+        <Link to="/">
+          <img className="logo" src="../logo.png" />
+        </Link>
+      </div>
+
+      {/* Responsive menu button */}
+      <div class="nav-btn">
+        {/* Label for the menu button */}
+        <label for="nav-check">
+          {/* Three spans creating the button icon */}
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+      </div>
