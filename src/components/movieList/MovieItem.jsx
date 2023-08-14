de82ca8764e1af54c96289ef3976f9c248c1b324
Dsimon2 <browsedavid@gmail.com>
2023-08-14 19:39:25 -0400

    Fetch movies (API)

--- src/components/movieList/MovieItem.jsx
+++ src/components/movieList/MovieItem.jsx
@@ -7,4 +7,22 @@ const MovieList = () => {
     // State to store the list of movies
     const [movieList, setMovieList] = useState([]);
 
-    
+ // Extracting the "type" parameter from the route
+ const { type } = useParams();
+ 
+ // Fetch movies when the component mounts
+ useEffect(() => {
+    getMovies();
+}, []);
+
+// Fetch movies when the "type" parameter changes
+useEffect(() => {
+    getMovies();
+}, [type]);
+
+// Fetch movies from the API
+const getMovies = () => {
+    fetch(`https://api.themoviedb.org/3/movie/${type ? type : "top_rated"}?api_key=07bb03709d728fbe5f83a335b1d729a5&language=en-US`)
+        .then(res => res.json())
+        .then(data => setMovieList(data.results));
+};
