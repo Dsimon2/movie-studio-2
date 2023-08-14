af4e758c91bf6ea6e853f923244f7afe1d0e21fc
Dsimon2 <browsedavid@gmail.com>
2023-08-14 19:04:21 -0400

    Skeleton Animation

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom"
 const Cards = ({movie}) => {
 // State to manage loading state
     const [isLoading, setIsLoading] = useState(true)
-    
+
 // Simulate loading delay and update isLoading after 1.5 seconds
     useEffect(() => {
         setTimeout(() => {
@@ -14,6 +14,15 @@ const Cards = ({movie}) => {
         }, 1500)
     }, []) 
 
-
+return <>
+    {
+        isLoading
+        ?
+         // Display skeleton animation during loading
+        <div className="cards">
+            <SkeletonTheme color="#202020" highlightColor="#444">
+                <Skeleton height={200} duration={3} />
+            </SkeletonTheme>
+        </div>
 
 }
