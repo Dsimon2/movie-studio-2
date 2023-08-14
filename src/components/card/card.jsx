51a0ce52a9435f6ab9f29bbc12fcc50ae1e28991
Dsimon2 <browsedavid@gmail.com>
2023-08-14 19:01:52 -0400

    Loading State

---
+++ src/components/card/card.jsx
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from "react"
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton"
+import "./card.css"
+import { Link } from "react-router-dom"
+
+const Cards = ({movie}) => {
+// State to manage loading state
+    const [isLoading, setIsLoading] = useState(true)
+    
+// Simulate loading delay and update isLoading after 1.5 seconds
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 1500)
+    }, []) 
+
+
+
+}
