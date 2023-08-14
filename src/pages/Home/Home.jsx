3c3ce2c2fcefd2a6325c4adc3f70b9fb3b5ec5a2
Dsimon2 <browsedavid@gmail.com>
2023-08-14 19:47:07 -0400

    fetch popular movies

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,3 +8,10 @@ import MovieList from "../../components/movieList/MovieItem";
 const Home = () => {
     // State to store the list of popular movies
     const [Movies, setMovies] = useState([]);
+
+    // Fetch popular movies when the component mounts
+    useEffect(() => {
+        fetch("https://api.themoviedb.org/3/movie/popular?api_key=07bb03709d728fbe5f83a335b1d729a5&language=en-US")
+            .then(res => res.json())
+            .then(data => setMovies(data.results));
+    }, []);
